bc69c02b9ea481ad9dd111d45c34ce59842b0a54
bavandlajyothi <149916511+bavandlajyothi@users.noreply.github.com>
2023-11-04 19:31:33 -0700

    add create table method in big query

--- import-to-big-query.py
+++ import-to-big-query.py
@@ -9,6 +9,24 @@ SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 KEY_FILE_LOCATION = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 
+def createTable(table):
+  query = f"""CREATE TABLE `{table}` (
+  client_id STRING NOT NULL,
+  activity_time STRING,
+  activity_type STRING,
+  campaign STRING,
+  channel_grouping STRING,
+  hostname STRING,
+  keyword STRING,
+  landing_page STRING,
+  medium STRING,
+  page_view ARRAY<STRING>,
+  source STRING)
+  """
+  r = bq.query(query)
+  print(r.result())
+
+
 def main(viewId, table, dateRange):
 
 if __name__ == '__main__':
